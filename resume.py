7f16f2bd8b1f999de2deb2a62af6745ce5e371b5
NurdauletAlibay <alibay.n01@mail.ru>
2022-10-12 13:02:13 +0600

    lab5

--- resume.py
+++ resume.py
@@ -3,17 +3,16 @@ date_of_birth = "Дата рождения : 30.09.2001"
 education = "Образование : Satbayev University"
 print(full_name)
 print(date_of_birth)
+
 education = {
     'ВУЗ': 'Satbayev University',
     'Специальность ': 'Computer Science',
-    'Год окончания ': '2023'
-}
+    'Год окончания ': '2023' }
 for x, y in education.items():
     print(x + ' : ' + y)
-
-skills = ['C++, Python', 'OS Linux', 'HTML,CSS']
-for x in skills:
-    print("Навыки : " + x)
+    skills = ['C++, Python', 'OS Linux', 'HTML,CSS']
+        for x in skills:
+        print("Навыки : " + x)
 
 info = []
 info.append("Номер телефона : ")
